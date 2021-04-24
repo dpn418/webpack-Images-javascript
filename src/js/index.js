81090f76516f6a4ea0bf66f0b218405d55a9026e
dpn418 <60945105+dpn418@users.noreply.github.com>
2021-04-23 21:18:06 -0400

    javascript init

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,31 @@
-import TestJS from './TestJs.js';
+"use strict";
+import ConsoleLog from './ConsoleLog.js';
+import alertMe from "./AlertMe";
 
-TestJS();
+(function(){
+    alertMe('Keith');
+    ConsoleLog(document);
+    window.onload = function() {
+
+        // create a couple of elements in an otherwise empty HTML page
+        const heading = document.createElement("h1");
+        const heading_text = document.createTextNode("Big Head! 3");
+        const heading2 = document.createElement("h1");
+        const heading_text2 = document.createTextNode("Big Head! 3");
+        heading.appendChild(heading_text);
+        heading.style.color = "green";
+        document.body.appendChild(heading);
+        heading2.appendChild(heading_text2);
+        document.body.appendChild(heading2);
+        heading2.style.color = "red";
+        heading2.style.border = "solid 1px black";
+        heading2.onclick = function () {
+            heading2.innerText = "test";
+        };
+    }
+})();
+/*
+
+
+ */
+
